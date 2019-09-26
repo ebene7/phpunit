7603cddac125014754fb2db0185795542796ab7d
dbarty <nixspam@web.de>
2019-09-27 00:37:55 +0200

    Add template test for testing getters and setters

--- src/E7/PHPUnit/Traits/OopTrait.php
+++ src/E7/PHPUnit/Traits/OopTrait.php
@@ -15,4 +15,30 @@ trait OopTrait
     {
         static::assertThat($object, new ObjectHasMethod($method), $message);
     }
+
+    /**
+     * @param type $object
+     * @param string $property
+     * @param mixed $value
+     * @param array $options
+     *
+     * @return void
+     */
+    protected function doTestGetterAndSetter(
+        $object,
+        string $property,
+        mixed $value = null,
+        array $options = []
+    ) {
+        $value = $value ?: md5(rand(1, 10000));
+
+        $getter = 'get' . $property;
+        $setter = 'set' . $property;
+
+        $this->assertObjectHasMethod($getter, $object);
+        $this->assertObjectHasMethod($setter, $object);
+
+        $this->assertEquals($object, call_user_func([$object, $setter], $value));
+        $this->assertEquals($value, call_user_func([$object, $getter]));
+    }
 }
